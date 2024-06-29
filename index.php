365786365d68798ca43cc23fef18fefe592b3ffc
Kalindu Suraj <GADSE232F-006@student.nibm.lk>
2024-06-29 10:42:27 +0530

    Add Files
    Add the Sign In page, Sign Up Page, ad Create Index File And Forgot Password Option

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Style Sheet -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/FooterStyle.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+
+    <!-- Bootstrap Bundle JS (includes Popper.js) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>EzBus</title>
+</head>
+<div class="container-fluid navbarColor">
+    <nav class="container navbar justify-content pt-3">
+        <div>
+            <a href="index.php"><img class="logo" src="src/Logo.png" alt=""></a>
+        </div>
+        <!-- 
+            <div>
+            <h1 class="logoTitle ">MovieLK</h1>
+            </div>
+         -->
+        <div class="justify-content-end">
+            <a href="signIn.php"><button class="btns ">LogIn</button></a>
+            <a href="signUp.php"><button class="btns ">SignUp</button></a>
+        </div>
+    </nav>
+</div>
+
+<!------------------------------------------------------------------------------>
+<body>
+
+</body>
+
+<!------------------------------------------------------------------------------>
+<footer class="footer container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+
+            </div>
+            <div class="col-4 ">
+
+            </div>
+            <div class="col-3 ">
+
+            </div>
+
+        </div>
+    </div>
+    <div class="CopyRightDiv">
+        <p class="CopyRight"> EzBus &copy; 2024</p>
+    </div>
+</footer>
+
+</html>
